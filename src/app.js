1c145a7360ac01869d8cac2ae5ea7a3a9be971aa
Hardik <hdnakarani@gmail.com>
2019-05-23 17:18:32 +0530

    setup reducer

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 import { SafeAreaView, Text } from "react-native";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
-
+import reducers from "./reducers";
 class App extends Component {
   render() {
     return (
-      <Provider store={createStore()}>
+      <Provider store={createStore(reducers)}>
         <View>
           <Text>Hello!</Text>
         </View>
