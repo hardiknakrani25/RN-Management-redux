c583759ee835e80744d79d8ef7cce73920841ae0
Hardik <hdnakarani@gmail.com>
2019-05-23 17:33:05 +0530

    setup firebase initialize

--- src/app.js
+++ src/app.js
@@ -3,17 +3,12 @@ import { SafeAreaView, Text } from "react-native";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducers from "./reducers";
+import { firebaseConfig } from "../config";
+import firebase from "firebase";
+
 class App extends Component {
   componentWillMount() {
-    const firebaseConfig = {
-      apiKey: "AIzaSyCTkK0yqS9RGx_KFnGKiuwMfnr85wU-StA",
-      authDomain: "manager-2e89c.firebaseapp.com",
-      databaseURL: "https://manager-2e89c.firebaseio.com",
-      projectId: "manager-2e89c",
-      storageBucket: "manager-2e89c.appspot.com",
-      messagingSenderId: "895752016128",
-      appId: "1:895752016128:web:52ce8b3b111d0a78"
-    };
+    firebase.initializeApp(firebaseConfig);
   }
 
   render() {
