15fa9c88ce32a73205431a558406b489ac3e2448
Hardik <hdnakarani@gmail.com>
2019-05-25 14:11:28 +0530

    Grouping scenes

--- src/router.js
+++ src/router.js
@@ -7,9 +7,17 @@ import EmployeeList from "./component/employeeList";
 const RouterComponent = () => {
   return (
     <Router>
-      <Scene key="root">
-        <Scene key="login" component={LoginForm} title="Please login" />
-        <Scene key="employeeList" component={EmployeeList} title="Employees" />
+      <Scene key="root" hideNavBar>
+        <Scene key="auth">
+          <Scene key="login" component={LoginForm} title="Please login" />
+        </Scene>
+        <Scene key="main">
+          <Scene
+            key="employeeList"
+            component={EmployeeList}
+            title="Employees"
+          />
+        </Scene>
       </Scene>
     </Router>
   );
