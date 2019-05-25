ddd89cf7ad369e6de61e4d35193a6a937dc44e73
Hardik <hdnakarani@gmail.com>
2019-05-25 15:57:42 +0530

    Picker and style override using platform specific

--- src/component/employeeCreate.js
+++ src/component/employeeCreate.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
-import { Picker } from "react-native";
+import { Picker, Text, Platform } from "react-native";
 import { Card, CardSection, Input, Button } from "./common";
 import { connect } from "react-redux";
 import { employeeUpdate } from "../actions";
 class EmployeeCreate extends Component {
+  shiftView() {
+    if (Platform.OS === "ios") {
+      return <Text style={style.pickerText}>Shift</Text>;
+    }
+  }
   render() {
     return (
       <Card>
@@ -29,8 +34,10 @@ class EmployeeCreate extends Component {
           />
         </CardSection>
         <CardSection>
+          {this.shiftView()}
+
           <Picker
-            style={{ flex: 1 }}
+            style={{ flex: 1, flexDirection: "column" }}
             selectedValue={this.props.shift}
             onValueChange={value =>
               this.props.employeeUpdate({ props: "shift", value })
@@ -56,6 +63,16 @@ class EmployeeCreate extends Component {
 const mapStateToProps = state => {
   const { name, phone, shift } = state.employeeForm;
   return { name, phone, shift };
+};
+
+const style = {
+  pickerText: {
+    fontSize: 18,
+    paddingLeft: 20,
+    position: "absolute",
+    marginTop: 16,
+    marginLeft: 5
+  }
 };
 
 export default connect(
